6a0a357332d3ac23847ed18a52737e6205f6dbf4
AmirHossein Ma <marjani.amir@gmail.com>
2012-07-16 21:42:20 +0430

    Uploading simple PhoneBook Repo

---
+++ App_Code/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Person
+/// </summary>
+public class Person
+{
+    #region Constractor
+    public Person()
+    {
+    }
+
+    public Person(int id,string firstName, string lastName, string telephone)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Telephone = telephone;
+    }
+    #endregion
+
+    #region Prop
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Telephone { get; set; }
+    #endregion
+
+    #region Methods
+    public string addPerson(string firstName, string lastName, string Telephone)
+    {
+        string Id = null;
+        return Id;
+    }
+
+    public List<Person> getPersons()
+    {
+        List<Person> collection = new List<Person>();
+        return collection;
+    }
+
+    public void removePerson(string Id)
+    {
+    }
+    #endregion
+
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection();
+        con.ConnectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=persons;Data Source=.\sqlexpress";
+        con.Open();
+        SqlCommand cmd = new SqlCommand("getAllPersons",con);
+        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+        SqlDataReader read = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+        List<Person> collection = new List<Person>();
+
+        while(read.Read())
+        {
+            collection.Add(new Person(
+                read["Id"] == DBNull.Value ? Int16.MinValue : (int)read["Id"],
+                read["FName"] == DBNull.Value ? string.Empty : (string)read["FName"],
+                read["LName"] == DBNull.Value ? string.Empty : (string)read["Lname"],
+                read["Tel"] == DBNull.Value ? string.Empty : (string)read["Tel"]
+                ));
+        }
+        var a = collection;
+    }
+}
